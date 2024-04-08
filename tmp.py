b91c8f8c676d8dee86d708c835c1eed4b82b1e72
jMujunen <jMujunen@github.com>
2024-04-08 15:25:27 -0700

    Removed .venv/ from remote

---
+++ tmp.py
@@ -0,0 +1,11 @@
+def test():
+    """
+    Edit a row in the table based on the given index.
+
+    Parameters:
+        index (int): The index of the row to be edited.
+
+    Returns:
+        None
+    """
+    print("tests/")
